bc3a855ba2f51b3afd7d6feebd8d80ff24dbe883
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-09-26 14:18:51 +1300

    add rand dependency

--- target/debug/guessing_game.d
+++ target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/home/jono/projects/guessing_game/target/debug/guessing_game: /home/jono/projects/guessing_game/src/main.rs
+/home/jono/rustprojects/rust_projects/guessing_game/guessing_game/target/debug/guessing_game: /home/jono/rustprojects/rust_projects/guessing_game/guessing_game/src/main.rs
