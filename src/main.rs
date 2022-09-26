16338424dd186a3c96176c9f7172e2995557792a
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-09-26 15:02:28 +1300

    code guess function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 //import standard I/O lib
 use std::io;
+//import random
+use rand::Rng;
+//ordering
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
+    
+    //4. Random number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    //display random number for testing - delete after
+    println!("The secret number is: {secret_number}");
 
     //1.take guess input
     println!("Please input your guess:");
@@ -19,5 +29,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    //5. Convert guess to int with validation
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    //5. Comparing guesses using match keyword
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low"),
+        Ordering::Greater => println!("Too high"),
+        Ordering::Equal => println!("Winner"),
+    }
+
 }
